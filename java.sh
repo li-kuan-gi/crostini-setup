8c84a15ec1897c973a92a7136a5f2f148d8f653f
li-kuan-gi <82380617+li-kuan-gi@users.noreply.github.com>
2024-03-17 17:02:25 +0800

    add "run java tests" shortcut

--- java.sh
+++ java.sh
@@ -12,4 +12,7 @@
 	# Settings
 	jq '. + { "java.inlayHints.parameterNames.enabled": "none" }' ~/.config/Code/User/settings.json > tmp.$$.json && mv tmp.$$.json ~/.config/Code/User/settings.json
 
+ 	# Keybindings
+  	jq '. += [{ "key": "ctrl+r ctrl+t","command": "java.test.editor.run" }]' ~/.config/Code/User/keybindings.json > tmp.$$.json && mv tmp.$$.json ~/.config/Code/User/keybindings.json
+
 }
